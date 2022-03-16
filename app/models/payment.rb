f38736537c2c41a196402569db50e6c647e596ac
ShahierNashaat <shahiernashaat97@gmail.com>
2022-03-16 09:30:58 +0200

    Add delete to payments and groups

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :user
-  has_many :payment_groups
+  has_many :payment_groups, dependent: :destroy
 
   validates :name, presence: true
   validates :amount, numericality: { greater_than: 0 }
